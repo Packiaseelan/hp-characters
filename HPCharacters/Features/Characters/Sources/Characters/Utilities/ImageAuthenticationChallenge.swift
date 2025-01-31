1054cac06ac0f86181102d10dd8233023014fd4f
Packiaseelan Socretes <packiaseelan.socretes@publicissapient.com>
2025-01-31 13:46:53 +0530

    ImageAuthenticationChallenge class added to authentication for image download

---
+++ HPCharacters/Features/Characters/Sources/Characters/Utilities/ImageAuthenticationChallenge.swift
@@ -0,0 +1,19 @@
+import Foundation
+import Kingfisher
+
+final public class ImageAuthenticationChallenge: AuthenticationChallengeResponsible {
+    
+    public init() {}
+    
+    public func downloader(_ downloader: ImageDownloader, didReceive challenge: URLAuthenticationChallenge,
+                    completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
+            if let serverTrust = challenge.protectionSpace.serverTrust {
+                let credential = URLCredential(trust: serverTrust)
+                completionHandler(.useCredential, credential)
+                return
+            }
+        }
+        completionHandler(.performDefaultHandling, nil)
+    }
+}
--- HPCharacters/HPCharactersApp.swift
+++ HPCharacters/HPCharactersApp.swift
@@ -4,18 +4,21 @@ import Shared
 import Characters
 import NavigationManager
 import NetworkManager
+import Kingfisher
 
 @main
 struct HPCharactersApp: App {
     
     /// An instance of `AppInitializer` used to perform initial setup tasks for the app.
     private let initializer: AppInitializer = AppInitializer()
+    private let imageAuthenticationChallenge = ImageAuthenticationChallenge()
     
     /// Initializes a new instance of `HPCharactersApp`.
     /// This initializer sets up the navigation manager and performs initial app setup.
     init() {
         // Perform initial setup tasks using the initializer.
         self.initializer.initialize()
+        ImageDownloader.default.authenticationChallengeResponder = imageAuthenticationChallenge
     }
     
     var body: some Scene {
